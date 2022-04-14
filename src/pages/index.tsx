867590b868e04bf6f1be4d4b7f98a2288c4634c9
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-14 17:43:54 -0300

    Project: global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import styles from '../styles/global.module.scss'
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello world</h1>
+    <>
+      <Head>
+        <title>Ig.news</title>
+      </Head>
+      <h1>
+        
+        Hello world
+      </h1>
+    </>   
   )
 }
