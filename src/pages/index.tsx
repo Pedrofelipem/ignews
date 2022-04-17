47c0c0176d1df13d4f350132c1f0b18f2da8c293
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-17 17:00:38 -0300

    components: buttom {

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton/SubscribeButton'
 
 import styles from './home.module.scss'
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Head>
         <title>Home | Ig.news</title>
       </Head>
+      
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
@@ -17,7 +19,9 @@ export default function Home() {
             Get access to all the publications <br /> 
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
+
         <Image src="/images/avatar.svg" alt="Girl coding" height={500} width={500}/>
       </main>
     </>   
