0da5dcc52ad8690f285c962fe8822d31b8941457
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-25 18:45:27 -0300

    subscribe: Generating checkout session

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,11 +1,23 @@
+import { signIn, useSession } from 'next-auth/react';
 import { SubscribeButtonProps } from '../../models/subscribeButtonProps';
 import styles from './styles-subscribeButton.module.scss'
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+    const {data : session} = useSession();
+
+    function hendleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+    }
+    
     return(
         <button 
             type="button"
             className={styles.subscribeButton}
+            onClick={hendleSubscribe}
         >
             Subscribe now
         </button>
